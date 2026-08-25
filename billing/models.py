837f8103ba9aea4399e623f9d7a1490433f552c2
Suthar Ravikumar k <ravisuthar1820@gmail.com>
2026-08-25 09:22:55 +0530

    Customer model create

--- billing/models.py
+++ billing/models.py
@@ -33,5 +33,5 @@ class Customer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} ({self.phone})"
+        return self.name
 
