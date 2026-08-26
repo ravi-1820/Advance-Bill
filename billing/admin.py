7aedac892fa3d7e4a93c191d330788cc359d14ff
Suthar Ravikumar k <ravisuthar1820@gmail.com>
2026-08-26 09:04:08 +0530

    Redesign distributor dashboard with Shadcn UI aesthetics

--- billing/admin.py
+++ billing/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from billing.models import User, OTP
+from billing.models import User, OTP, Customer
 
 admin.site.register(User)
 admin.site.register(OTP)
+admin.site.register(Customer) 
+  
